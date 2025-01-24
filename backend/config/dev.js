f72d3cd45f9bec8b21a9c7fac29f6ffa95724328
Saikrishnanumula <89066316+Saikrishnanumula@users.noreply.github.com>
2025-01-24 20:09:42 +0530

    Stage 1

---
+++ backend/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI : process.env.MONGO_URI, // These are declared in .env file
+    cookieKey : process.env.cookieKey
+}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleSignUp = () =>{
+  const LoginButton = () => {
+    navigate("/dashboard");
+  };
+
+  const handleSignUp = () => {
     navigate("/signup");
   };
   return (
     <>
-    <div>WELCOME TO <i>HABIT TRACKER</i></div>
-    <p>A ONE PLACE TO TRACK YOUR HABITS</p>
-    <div>Login Page</div>
-    <p>Enter your detils to continue</p>
-    USERNAME <input type="text" placeholder='username' ></input><br /><br />
-    PASSWORD <input type="password" placeholder='password' ></input> <br /><br />
-    <div><button>Login</button></div>
-    <p>OR </p>
-    <button onClick={handleSignUp}>SIGN UP</button>
-    <div>
-        <p>WE CAN TRAVEL AS FAR AS WE CAN BUT WE SHOULD WALK INSTEAD OF THINK.</p>
+      <div>
+        WELCOME TO <i>HABIT TRACKER</i>
+      </div>
+      <p>A ONE PLACE TO TRACK YOUR HABITS</p>
+      <div>Login Page</div>
+      <p>Enter your detils to continue</p>
+      USERNAME <input type="text" placeholder="username"></input>
+      <br />
+      <br />
+      PASSWORD <input type="password" placeholder="password"></input> <br />
+      <br />
+      <div>
+        <button onClick={LoginButton}>Login</button>
+      </div>
+      <p>OR </p>
+      <button onClick={handleSignUp}>SIGN UP</button>
+      <div>
+        <p>
+          WE CAN TRAVEL AS FAR AS WE CAN BUT WE SHOULD WALK INSTEAD OF THINK.
+        </p>
         <p>START TODAY BUDDY TRACK YOUR HABITS!</p>
         <p> §HABITS = YOU </p>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
